9656caae4499b599ce9e713991535d20a8c30c30
vinothrobinson <vinothrobinson03@gmail.com>
2023-01-25 08:54:16 -0500

    Add branches and matplotlib
    Create branches for each group member and add the python graphing
    library to the file.

--- bad_sorts.py
+++ bad_sorts.py
@@ -3,6 +3,7 @@ This file corresponds to the first graded lab of 2XC3.
 Feel free to modify and/or add functions to this file.
 """
 import random
+import matplotlib.pyplot as plot
 
 
 # Create a random list length "length" containing whole numbers between 0 and max_value inclusive
