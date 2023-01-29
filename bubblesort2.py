fcb5785185d83e61308158d754203cf8f1bc4535
vinothrobinson <vinothrobinson03@gmail.com>
2023-01-29 09:21:27 -0500

    Improve bubblesort2

--- bubblesort2.py
+++ bubblesort2.py
@@ -15,12 +15,12 @@ def bubble_sort(L):
 def bubblesort2(L):
     for i in range(len(L)):
         temp = L[0]
-        for j in range(len(L) - 1):
+        for j in range(len(L) - 1 - i):
             if temp < L[j+1]:
                 L[j] = temp
                 temp = L[j+1]
                 continue
             if temp > L[j+1]:
                 L[j] = L[j+1]
-                if (j+1) == len(L) - 1:
+                if (j+1) == len(L) - 1 - i:
                     L[j+1] = temp
