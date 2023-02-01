01d7119be6924909308e564431ccc3ff588b4d67
Brandon Bosman <brandonb@wightman.ca>
2023-02-01 08:47:52 -0500

    Added file for double pivot quicksort

---
+++ double_quicksort.py
@@ -0,0 +1,17 @@
+def quicksort(L):
+    copy = quicksort_copy(L)
+    for i in range(len(L)):
+        L[i] = copy[i]
+
+
+def quicksort_copy(L):
+    if len(L) < 2:
+        return L
+    pivot = L[0]
+    left, right = [], []
+    for num in L[1:]:
+        if num < pivot:
+            left.append(num)
+        else:
+            right.append(num)
+    return quicksort_copy(left) + [pivot] + quicksort_copy(right)
